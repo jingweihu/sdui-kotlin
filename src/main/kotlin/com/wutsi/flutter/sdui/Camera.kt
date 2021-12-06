549181bf80374d2b7747d2035ca5e7c93428ed7a
wutsi <herve.tchepannou@wutsi.com>
2021-12-06 08:10:54 -0500

    add lens direction

--- src/main/kotlin/com/wutsi/flutter/sdui/Camera.kt
+++ src/main/kotlin/com/wutsi/flutter/sdui/Camera.kt
@@ -1,11 +1,13 @@
 package com.wutsi.flutter.sdui
 
+import com.wutsi.flutter.sdui.enums.CameraLensDirection
 import com.wutsi.flutter.sdui.enums.WidgetType
 
 class Camera(
     val name: String,
     val uploadUrl: String,
     val action: Action? = null,
+    val lensDirection: CameraLensDirection? = null
 ) : WidgetAware {
     override fun toWidget() = Widget(
         type = WidgetType.Camera,
@@ -13,6 +15,7 @@ class Camera(
         attributes = mapOf(
             "name" to name,
             "uploadUrl" to uploadUrl,
+            "lensDirection" to lensDirection
         )
     )
 }
--- src/test/kotlin/com/wutsi/flutter/sdui/CameraTest.kt
+++ src/test/kotlin/com/wutsi/flutter/sdui/CameraTest.kt
@@ -1,6 +1,7 @@
 package com.wutsi.flutter.sdui
 
 import com.wutsi.flutter.sdui.enums.ActionType
+import com.wutsi.flutter.sdui.enums.CameraLensDirection
 import com.wutsi.flutter.sdui.enums.WidgetType
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -12,6 +13,7 @@ internal class CameraTest {
         val obj = Camera(
             uploadUrl = "http://1.1.1.1/upload",
             name = "Yo",
+            lensDirection = CameraLensDirection.external,
             action = Action(
                 type = ActionType.Prompt
             ),
@@ -22,9 +24,10 @@ internal class CameraTest {
         Assertions.assertEquals(WidgetType.Camera, widget.type)
         Assertions.assertEquals(obj.action, widget.action)
 
-        Assertions.assertEquals(2, widget.attributes.size)
+        Assertions.assertEquals(3, widget.attributes.size)
         Assertions.assertEquals(obj.name, widget.attributes["name"])
         Assertions.assertEquals(obj.uploadUrl, widget.attributes["uploadUrl"])
+        Assertions.assertEquals(obj.lensDirection, widget.attributes["lensDirection"])
 
         Assertions.assertEquals(0, widget.children.size)
     }
