4d0aeaddb02279da35f267e927c15241c8e1530f
wutsi <herve.tchepannou@wutsi.com>
2021-12-28 01:56:13 -0500

    add child

--- src/main/kotlin/com/wutsi/flutter/sdui/Center.kt
+++ src/main/kotlin/com/wutsi/flutter/sdui/Center.kt
@@ -2,8 +2,11 @@ package com.wutsi.flutter.sdui
 
 import com.wutsi.flutter.sdui.enums.WidgetType
 
-class Center : WidgetAware {
+class Center(
+    val child: WidgetAware? = null
+) : WidgetAware {
     override fun toWidget() = Widget(
         type = WidgetType.Center,
+        children = if (child == null) emptyList() else listOf(child.toWidget())
     )
 }
