57c7ce98c2e7c43e000aa690b61014c33b8f2858
wutsi <herve.tchepannou@wutsi.com>
2022-03-03 12:11:33 -0500

    improve radio-group

--- src/main/kotlin/com/wutsi/flutter/sdui/RadioGroup.kt
+++ src/main/kotlin/com/wutsi/flutter/sdui/RadioGroup.kt
@@ -3,16 +3,22 @@ package com.wutsi.flutter.sdui
 import com.wutsi.flutter.sdui.enums.WidgetType
 
 data class RadioGroup(
+    val separator: Boolean? = null,
+    val separatorColor: String? = null,
     val name: String,
     val value: String? = null,
-    val children: List<Radio>
+    val children: List<Radio>,
+    val action: Action? = null
 ) : WidgetAware {
     override fun toWidget() = Widget(
         type = WidgetType.RadioGroup,
         attributes = mapOf(
             "name" to name,
             "value" to value,
+            "separator" to separator,
+            "separatorColor" to separatorColor,
         ),
-        children = children.map { it.toWidget() }
+        children = children.map { it.toWidget() },
+        action = action
     )
 }
--- src/test/kotlin/com/wutsi/flutter/sdui/RadioGroupTest.kt
+++ src/test/kotlin/com/wutsi/flutter/sdui/RadioGroupTest.kt
@@ -3,7 +3,7 @@ package com.wutsi.flutter.sdui
 import com.wutsi.flutter.sdui.enums.WidgetType
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNull
+import kotlin.test.assertNotNull
 
 internal class RadioGroupTest {
     @Test
@@ -11,17 +11,22 @@ internal class RadioGroupTest {
         val group = RadioGroup(
             name = "foo",
             value = "1",
-            children = listOf(Radio("1"), Radio("2"))
+            separator = true,
+            separatorColor = "xxx",
+            children = listOf(Radio("1"), Radio("2")),
+            action = Action()
         )
 
         val widget = group.toWidget()
 
         assertEquals(WidgetType.RadioGroup, widget.type)
-        assertNull(widget.action)
+        assertNotNull(widget.action)
 
-        assertEquals(2, widget.attributes.size)
+        assertEquals(4, widget.attributes.size)
         assertEquals(group.name, widget.attributes["name"])
         assertEquals(group.value, widget.attributes["value"])
+        assertEquals(group.separator, widget.attributes["separator"])
+        assertEquals(group.separatorColor, widget.attributes["separatorColor"])
 
         assertEquals(2, widget.children.size)
     }
